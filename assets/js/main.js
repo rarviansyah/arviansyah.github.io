19a698830a1550a3cd499668254d41d668e31d88
arviansyah <arviansyahwahyur@gmail.com>
2022-07-30 00:58:27 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,14 @@ if (navClose) {
 }
 
 // REMOVE MENU MOBILE
+const navlink = document.querySelectorAll('.nav__link');
+
+function linkaAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // when we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navlink.forEach((n) => n.addEventListener('click', linkaAction));
 
 // ACCORDION SKILLS
 
