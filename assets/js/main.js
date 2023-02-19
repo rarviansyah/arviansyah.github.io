b3a7ff5cc074e0e9b6d7f3d8288a181ebe8040c7
arviansyah <arviansyahwahyur@gmail.com>
2023-02-19 22:53:20 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,108 +1,109 @@
 /*MENU SHOW Y HIDDEN*/
 const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 /*MENU SHOW*/
 /*Validate if constant exists*/
 if (navToggle) {
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu');
-    });
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
 }
 
 /*MENU HIDDEN*/
 /*Validate if constant exists*/
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu');
-    });
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
 }
 
 /*===== REMOVE MENU MOBILE =====*/
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu');
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*====== Accordion skills ======*/
 const skillsContent = document.getElementsByClassName('skills__content'),
-    skillsHeader = document.querySelectorAll('.skills__header');
+  skillsHeader = document.querySelectorAll('.skills__header');
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className;
+  let itemClass = this.parentNode.className;
 
-    for (i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close';
-    }
-    if (itemClass === 'skills__content skills__close') {
-        this.parentNode.className = 'skilss__content skills__open';
-    }
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__close';
+  }
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skilss__content skills__open';
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills);
+  el.addEventListener('click', toggleSkills);
 });
 
 /*===============QUALIFICATION TABS===========*/
 const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]');
+  tabContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach((tab) => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.target);
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach((tabContent) => {
-            tabContent.classList.remove('qualification__active');
-        });
-        target.classList.add('qualification__active');
-
-        tabs.forEach((tab) => {
-            tab.classList.remove('qualification__active');
-        });
-        tab.classList.add('qualification__active');
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('qualification__active');
     });
+    target.classList.add('qualification__active');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification__active');
+    });
+    tab.classList.add('qualification__active');
+  });
 });
 
 /*======= SERVICE MODAL ========*/
 const modalViews = document.querySelectorAll('.services__modal'),
-    modalBtns = document.querySelectorAll('.services__button'),
-    modalCloses = document.querySelectorAll('.services__modal-close');
+  modalBtns = document.querySelectorAll('.services__button'),
+  modalCloses = document.querySelectorAll('.services__modal-close');
 
-let modal = function(modalClick) {
-    modalViews[modalClick].classList.add('active-modal');
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add('active-modal');
 };
 
 modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener('click', () => {
-        modal(i);
-    });
+  modalBtn.addEventListener('click', () => {
+    modal(i);
+  });
 });
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click', () => {
-        modalViews.forEach((modalView) => {
-            modalView.classList.remove('active-modal');
-        });
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal');
     });
+  });
 });
 
 /*====== PORTFOLIO SWIPER ======*/
 let swiper = new swiper('.portfolio__container', {
-    cssMode: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    mousewheel: true,
-    keyboard: true,
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
 
 /*======== TESTIMONIAL =======*/
