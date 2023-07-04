87b7df10cfb55cfb46004234abaee3a25ea71e47
arviansyah <arviansyahwahyur@gmail.com>
2023-07-04 17:10:52 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) => {
 });
 
 /*====== PORTFOLIO SWIPER ======*/
-let swiper = new Swiper('.portfolio_container', {
+let swiperPortfolio = new Swiper('.portfolio_container', {
   cssMode: true,
   loop: true,
 
@@ -107,7 +107,17 @@ let swiper = new Swiper('.portfolio_container', {
 });
 
 /*======== TESTIMONIAL =======*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
 
+  pagination: {
+    el: '.swiper-pagination',
+    clickabel: true,
+    dynamicBullets: true,
+  },
+});
 /*============ SCROLL SECTIONS ACTIVE LINK =============*/
 
 /*========= CHANGE BACKGOUND HEADER ==========*/
